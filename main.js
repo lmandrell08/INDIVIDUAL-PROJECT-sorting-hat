2d43dfc816447b2f944c39dd8e1e4894b9e020a0
lmandrell08 <lmandrell08@gmail.com>
2022-10-29 07:26:12 -0500

    forgot to push up or deploy last night

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ const renderToDom = (divId, htmlToRender) => {
   selectedDiv.innerHTML= htmlToRender;
 };
 //cards on the DOM
-const cardsOnDom = (arr) => {
+const sortingHat = (arr) => {
+  console.log();
   let domString = `<div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">Welcome to your new home!</h5>
@@ -20,16 +21,32 @@ const cardsOnDom = (arr) => {
   renderToDom("#sortingHat", domString);
 };
 
+sortingHat();
+
+//form
+
+const studentForm = () => {}
+  
+  let domString = `
+  <form id="studentNameForm">
+  <div class="mb-3">
+  <label for="studentName" class="form-label">Student Name</label>
+  <input type="text" placeholder="Enter your name here." class="form-control" id="studentName">
+</div>
+<button type="submit" id="submitButton" class="btn btn-primary">Submit</button>
+</form>
+`;
+  
+  renderToDom("#studentForm", domString);
+
+
+studentForm();
+
+
 
 //button
-
-const button = () => {
-  document.querySelector(`body`).addEventListener("click", buttonControl);
-  document.querySelector(`#students`)
-  document.addEventListener("click", expelStudents);
-};
-
 const buttonControl = (event) => {
+  console.log();
   if (event.target.id === "startButton") {
     let domString = `
      <form id="studentNameForm">
@@ -42,24 +59,25 @@ const buttonControl = (event) => {
      `
 
     renderToDom("#studentForm", domString);
-  }
+  };
 
   if (event.target.id === "submitButton" && event.target.type === "submit") {
+    console.log(nameofStudent);
     const nameofStudent = document.getElementById("studentName").value;
-    if (nameofStudent === "") {
-      makeHowler();
-    } else {
-      event.preventDefault();
-      sortStudents();
-    }
+  
 
-    document.querySelector("form").reset();
+    document.querySelector("studentForm").reset();
   }
+
+
 };
+
+
 
 //student cards to page
 const placeStudents = (arr) => {
   let domString = " ";
+
 
   arr.forEach((student, i) => {
     domString += `
@@ -75,6 +93,7 @@ const placeStudents = (arr) => {
 
   renderToDom("#hogwartsStudents", domString);
 };
+
 
 //expels student to the dark army
 const expelStudents = (event) => {
@@ -94,4 +113,65 @@ const expelStudents = (event) => {
 };
 
 
+
 //randomNum function for sorting
+const sortStudents = () => {
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 4) + 1;
+  };
+
+  const studentHouse = randomNumber();
+
+  if (studentHouse === 1) {
+    const student = {
+      name: document.querySelector("#studentName").value,
+      house: "Gryffindor",
+      crest: `https://static.wikia.nocookie.net/pottermore/images/1/16/Gryffindor_crest.png`,
+      style: "grif-style",
+    };
+    newStudents.push(student);
+  }
+  if (studentHouse === 2) {
+    const student = {
+      name: document.querySelector("#studentName").value,
+      house: "Hufflepuff",
+      crest: `https://static.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png`,
+      style: "huff-style",
+    };
+    newStudents.push(student);
+  }
+  if (studentHouse === 3) {
+    const student = {
+      name: document.querySelector("#studentName").value,
+      house: "Ravenclaw",
+      crest: `https://static.wikia.nocookie.net/pottermore/images/4/4f/Ravenclaw_crest.png`,
+      style: "rave-style",
+    };
+    newStudents.push(student);
+  }
+  if (studentHouse === 4) {
+    const student = {
+      name: document.querySelector("#studentName").value,
+      house: "Slytherin",
+      crest: `https://static.wikia.nocookie.net/pottermore/images/4/45/Slytherin_Crest.png`,
+      style: "sly-style",
+    };
+    newStudents.push(student);
+  }
+
+  placeStudents(newStudents);
+};
+
+
+
+
+//starts app
+
+const init = () => {
+  sortingHat();
+  button();
+  placeStudents(newStudents);
+  sortDeathEaters(deathEaters);
+};
+
+init();
